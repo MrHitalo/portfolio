82f93f71e7f50f6827ae3b55c275a134b66e136b
MrHitalo <hitalofariaalmeida1602@gmail.com>
2025-05-10 13:07:11 -0300

    remoção

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-
-const configDataBase = {
-  hots: "database-2.cd8augmgutl0.us-east-2.rds.amazonaws.com",
-  port: 3306,
-  user: "admin",
-  password: "hitalo2001",
-};
--- frontend/src/service/api/ProductFake.ts
+++
@@ -1,8 +0,0 @@
-export async function getProducts() {
-  try {
-    const response = await fetch("https://fakestoreapi.com/products/");
-    return response.json();
-  } catch (erro) {
-    console.error("Erro com API", erro);
-  }
-}
